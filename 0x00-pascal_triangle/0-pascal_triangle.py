1799d6441cb2d50df51cc7ec85479c1b53d0487d
PeterALX <quaypeter25@gmail.com>
2024-10-05 17:56:38 +0300

    fixxxing pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
+"""
+Pascal's triangle
+"""
+
 
 def factorial(n):
+    """
+    return the factorial of a number
+    """
     result = 1
     for i in range(2, n + 1):
         result *= i
     return result
+
+
 def coefficient(n, k):
+    """
+    return the binomial coefficient(n, k)
+    """
     return int(factorial(n) / (factorial(k) * factorial(n - k)))
+
+
 def pascal_triangle(n):
+    """
+    compute and return pascal triangle of height n
+    """
     triangle = []
     for i in range(n):
         triangle.append([])
