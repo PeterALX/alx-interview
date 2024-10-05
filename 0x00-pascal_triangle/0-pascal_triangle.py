48d7b26a26a9bb92c81ad77f46eb6f3759e3496b
PeterALX <quaypeter25@gmail.com>
2024-10-05 17:41:59 +0300

    first attempt at pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+from math import factorial
+
+def coefficient(n, k):
+	return int(factorial(n) / (factorial(k) * factorial(n - k)))
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        for j in range(i + 1):
+            triangle[i].append(coefficient(i, j))
+    return(triangle)
