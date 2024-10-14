f80430add5f17bfa7bd5a2f8e1e152639bdb26ef
PeterALX <quaypeter25@gmail.com>
2024-10-14 13:50:37 +0300

    attempt at fixing maximum recursion depth

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-
 '''solution for lockboxes puzzle'''
 
 
 def open_box(key, unlocked, boxes):
+    '''
+    checks if keys in a box have been opened
+    if a key hasn't been opened,
+        adds the key to the set of unlocked boxes and opens it
+    it is called recursively until
+        either all boxes are open or no other boxes can be opened
+    no return value,
+        it modifies a set:unlocked to keep track of unlocked boxes
+    '''
+    toUnlock = set()
     for key in boxes[key]:
         if (key not in unlocked
                 and len(unlocked) < len(boxes)
                 and 0 <= key < len(boxes)):
             unlocked.add(key)
-            open_box(key, unlocked, boxes)
-        else:
-            pass
+            toUnlock.add(key)
+    for key in toUnlock:
+        open_box(key, unlocked, boxes)
 
 
 def canUnlockAll(boxes):
+    '''
+    returns true if all boxes can be opened from only the 0th box being opened
+    otherwise returns false
+    '''
     unlocked = {0}
     open_box(0, unlocked, boxes)
     if (len(unlocked) < len(boxes)):
--- 0x01-lockboxes/main.py
+++ 0x01-lockboxes/main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+boxes = []
+for i in range(1000):
+    boxes.append([])
+    for j in range(1000):
+        boxes[i].append(j)
+
+print(len(boxes[209]))
+
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
+print(canUnlockAll(boxes))  # true
 
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))  # true
@@ -14,6 +23,8 @@ boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))  # false
 print('_______________________________')
 
-boxes = [ [7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5], ] 
+boxes = [[7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3],
+         [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7],
+         [4, 2, 9, 6, 6, 5, 5], ]
 print(canUnlockAll(boxes))
 print('_______________________________')
